5fb91f0dfe7647e04c6c729b8fad419f00468ab2
Antonia <antonianomamiukor@gmail.com>
2020-06-24 15:25:48 -0700

    made a create method in movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,7 +21,29 @@ class MoviesController < ApplicationController
       )
   end
 
+  def create
+    movie = Movie.new(movie_params)
+    if new_movie.save
+      render(
+        status: ok,
+        json: @movie.as_json(
+          only: [:id, :title, :overview, :release_date, :image_url, :external_id, :available_inventory]
+        )
+      )
+    else
+      render json: {
+        ok: false,
+        errors: movie.errors.messages
+        }, status: :bad_request
+        return
+      end
+  end
+
   private
+  def movie_params
+    params.permit(:title, :overview, :release_date, :image_url, :external_id)
+  end
+
 
   def require_movie
     @movie = Movie.find_by(title: params[:title])
