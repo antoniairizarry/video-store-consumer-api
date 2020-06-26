f88604abda529a2284212e45942618b04234b82a
Antonia <antonianomamiukor@gmail.com>
2020-06-25 22:59:43 -0700

    added title validation to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
 
+  validates :title, presence: true, uniqueness: true
+
   def available_inventory
     self.inventory - Rental.where(movie: self, returned: false).length
   end
